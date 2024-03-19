57ec79d530cf760b8c47af57d5e67473cd59bb23
Nishan Gautam <144698632+nxhettry@users.noreply.github.com>
2024-03-19 21:06:18 +0545

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 const db = new pg.Client({
     user: "postgres",
-    password: "khuljasimsim",
+    password: "THISISPRIVATE",
     database: "world",
     host: "localhost",
     port: 5432,
